518c72827956ab997e932f17e05b645e023e58cf
Bitheral <me@bitheral.net>
2021-10-07 20:01:46 +0100

    Started work on Player movement and interaction with objects
    + Created basic character controller - Will no longer use "Character Movement Fundamentals" asset pack from the FPS Bundle.
    + Created basic house layout using floor plan and 3D models.
    + Created an assets scene to show model relative to player.
    + Added GameAssets folder to .gitignore to remove External assets from Humble Bundle being in the GitHub Repository.

---
+++ Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100;
+    
+    Transform playerBody;
+    float xRotation = 0;
+
+    // Interactivity
+    Ray ray;
+    RaycastHit hit;
+    GameObject lookingAt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = transform.parent;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Mouse Look
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(Vector3.up, mouseX);
+
+        // Raycast
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit, 2f, (1 << 7))) {
+            lookingAt = hit.transform.gameObject;
+            Debug.Log("Hit object! - " + lookingAt.name);
+        }
+    }
+}
